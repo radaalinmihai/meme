26dca4930135286caad2f4022937b054ac06b087
DontStealMyCodePls <n3gatyv95@gmail.com>
2019-08-07 17:09:13 +0300

    Made some changes xd

--- index.js
+++ index.js
@@ -5,5 +5,7 @@
 import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
+XMLHttpRequest = GLOBAL.originalXMLHttpRequest ? 
+  GLOBAL.originalXMLHttpRequest : GLOBAL.XMLHttpRequest;
 
 AppRegistry.registerComponent(appName, () => App);
